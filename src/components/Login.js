da743b91d8bd2b8b3219c0ad76001ace72f11141
unknown <vankur017@gmail.com>
2024-10-15 11:10:54 +0530

    made login page responsive

--- src/components/Login.js
+++ src/components/Login.js
@@ -101,55 +101,60 @@ const Login = () => {
 
   return (
     <div>
-      <Header/>
-      <div className='absolute h-screen w-full'>
-          <img className='absolute top-0 left-0 object-cover w-full h-full'
-            src={BACKGROUND_IMG} 
-            alt="Not rendered"
-            />
-            </div>
-      <form onSubmit={(e)=>e.preventDefault()} className='w-3/12 absolute p-16 bg-black mx-auto left-0 right-0 my-44 rounded-lg text-white opacity-85'>
-       
-       <h1 className='font-bold text-3xl mt-6 py-4 px-3 opacity-100'>
-        {isSignInForm ? 'Sign In' : 'Sign Up'}
-       </h1>
-       
-       {!isSignInForm && 
-       <input ref={name}  type='text' placeholder='Full Name' className='p-5 my-4 w-full  bg-gray-500'/>
-       }
-        
-
-        <input
-         ref={email}
-         type='email'
-         placeholder='Email' 
-         className='p-5 my-4  w-full bg-gray-500'
-         />
-        
-        <input 
-        ref={password} 
-        type='password' 
-        placeholder='Password' 
-        className='p-5 my-4 w-full  bg-gray-500'
-        />
-        <p className='text-red-500 font-bold text-lg py-2'>{errorMessage}</p>
-
-        <button 
-        className='p-4 my-6 rounded-2xl text-white bg-red-700 w-full' 
-        onClick={handleClick}>
-          {
-            isSignInForm ? 'Sign In' : 'Sign Up'
-          }
-          </button>
-        <p 
-        className='py-4 font-semibold hover:cursor-pointer' 
-        onClick={toggleSignInForm}
-        >
-          {isSignInForm ? 'New to Flix? Sign Up Now' : 'Already a User? Sign In Now'}
-        </p>
-      </form>
-     
+    <Header />
+    <div className='absolute h-screen w-full'>
+      <img
+        className='absolute top-0 left-0 object-cover w-full h-full'
+        src={BACKGROUND_IMG}
+        alt="Not rendered"
+      />
     </div>
+    <form 
+      onSubmit={(e) => e.preventDefault()} 
+      className='w-full sm:w-8/12 md:w-6/12 lg:w-4/12 xl:w-3/12 absolute p-8 sm:p-12 md:p-16 bg-black mx-auto left-0 right-0 my-44 rounded-lg text-white opacity-85'>
+      <h1 className='font-bold text-2xl sm:text-3xl lg:text-4xl mt-6 py-4 px-3 opacity-100'>
+        {isSignInForm ? 'Sign In' : 'Sign Up'}
+      </h1>
+  
+      {!isSignInForm && 
+        <input
+          ref={name}
+          type='text'
+          placeholder='Full Name'
+          className='p-3 sm:p-5 my-2 sm:my-4 w-full bg-gray-500'
+        />
+      }
+  
+      <input
+        ref={email}
+        type='email'
+        placeholder='Email'
+        className='p-3 sm:p-5 my-2 sm:my-4 w-full bg-gray-500'
+      />
+  
+      <input
+        ref={password}
+        type='password'
+        placeholder='Password'
+        className='p-3 sm:p-5 my-2 sm:my-4 w-full bg-gray-500'
+      />
+  
+      <p className='text-red-500 font-bold text-lg py-2'>{errorMessage}</p>
+  
+      <button
+        className='p-3 sm:p-4 my-4 sm:my-6 rounded-2xl text-white bg-red-700 w-full'
+        onClick={handleClick}>
+        {isSignInForm ? 'Sign In' : 'Sign Up'}
+      </button>
+  
+      <p
+        className='py-4 font-semibold hover:cursor-pointer'
+        onClick={toggleSignInForm}>
+        {isSignInForm ? 'New to Flix? Sign Up Now' : 'Already a User? Sign In Now'}
+      </p>
+    </form>
+  </div>
+  
   )
 }
 
